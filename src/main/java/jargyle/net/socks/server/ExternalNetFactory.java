296c43fe7e0e27c1b5028a9cac6b5eab1d5c9d8a
Jonathan K. Henderson <j0n4th4n.h3nd3rs0n@gmail.com>
2021-03-07 20:40:01 -0500

    replaced for loop used for casting Property objects

--- src/main/java/jargyle/net/socks/server/ExternalNetFactory.java
+++ src/main/java/jargyle/net/socks/server/ExternalNetFactory.java
@@ -28,15 +28,11 @@ import jargyle.util.Strings;
 
 final class ExternalNetFactory extends NetFactory {
 	
-	private static List<Property<Object>> castProperties(
-			final List<Property<? extends Object>> properties) {
-		List<Property<Object>> props = new ArrayList<Property<Object>>();
-		for (Property<? extends Object> property : properties) {
-			@SuppressWarnings("unchecked")
-			Property<Object> prop = (Property<Object>) property;
-			props.add(prop);
-		}
-		return props;
+	private static Property<Object> cast(
+			final Property<? extends Object> property) {
+		@SuppressWarnings("unchecked")
+		Property<Object> prop = (Property<Object>) property;
+		return prop;
 	}
 	
 	private final Configuration configuration;
@@ -88,64 +84,66 @@ final class ExternalNetFactory extends NetFactory {
 	
 	private List<Property<Object>> newSocks5ClientProperties() {
 		Settings settings = this.configuration.getSettings();
-		List<Property<? extends Object>> properties = 
-				new ArrayList<Property<? extends Object>>();
+		List<Property<Object>> properties =	new ArrayList<Property<Object>>();
 		if (settings.containsNondefaultValue(
 				SettingSpec.CHAINING_SOCKS5_AUTH_METHODS)) {
 			AuthMethods authMethods = settings.getLastValue(
 					SettingSpec.CHAINING_SOCKS5_AUTH_METHODS);
-			properties.add(PropertySpec.SOCKS5_AUTH_METHODS.newProperty(
-					authMethods));
+			properties.add(cast(PropertySpec.SOCKS5_AUTH_METHODS.newProperty(
+					authMethods)));
 		}
 		if (settings.containsNondefaultValue(
 				SettingSpec.CHAINING_SOCKS5_FORWARD_HOSTNAME_RESOLUTION)) {
 			Boolean forwardHostnameResolution = settings.getLastValue(
 					SettingSpec.CHAINING_SOCKS5_FORWARD_HOSTNAME_RESOLUTION);
-			properties.add(PropertySpec.SOCKS5_FORWARD_HOSTNAME_RESOLUTION.newProperty(
-					forwardHostnameResolution));
+			properties.add(cast(
+					PropertySpec.SOCKS5_FORWARD_HOSTNAME_RESOLUTION.newProperty(
+							forwardHostnameResolution)));
 		}
 		if (settings.containsNondefaultValue(
 				SettingSpec.CHAINING_SOCKS5_GSSAPI_MECHANISM_OID)) {
 			Oid gssapiMechanismOid = settings.getLastValue(
 					SettingSpec.CHAINING_SOCKS5_GSSAPI_MECHANISM_OID);
-			properties.add(PropertySpec.SOCKS5_GSSAPI_MECHANISM_OID.newProperty(
-					gssapiMechanismOid));
+			properties.add(cast(
+					PropertySpec.SOCKS5_GSSAPI_MECHANISM_OID.newProperty(
+							gssapiMechanismOid)));
 		}
 		if (settings.containsNondefaultValue(
 				SettingSpec.CHAINING_SOCKS5_GSSAPI_NEC_REFERENCE_IMPL)) {
 			Boolean gssapiNecReferenceImpl = settings.getLastValue(
 					SettingSpec.CHAINING_SOCKS5_GSSAPI_NEC_REFERENCE_IMPL);
-			properties.add(
+			properties.add(cast(
 					PropertySpec.SOCKS5_GSSAPI_NEC_REFERENCE_IMPL.newProperty(
-							gssapiNecReferenceImpl));
+							gssapiNecReferenceImpl)));
 		}
 		if (settings.containsNondefaultValue(
 				SettingSpec.CHAINING_SOCKS5_GSSAPI_PROTECTION_LEVELS)) {
 			GssapiProtectionLevels gssapiProtectionLevels =	settings.getLastValue(
 					SettingSpec.CHAINING_SOCKS5_GSSAPI_PROTECTION_LEVELS);
-			properties.add(
+			properties.add(cast(
 					PropertySpec.SOCKS5_GSSAPI_PROTECTION_LEVELS.newProperty(
-							gssapiProtectionLevels));
+							gssapiProtectionLevels)));
 		}
 		if (settings.containsNondefaultValue(
 				SettingSpec.CHAINING_SOCKS5_GSSAPI_SERVICE_NAME)) {
 			String gssapiServiceName = settings.getLastValue(
 					SettingSpec.CHAINING_SOCKS5_GSSAPI_SERVICE_NAME);
-			properties.add(PropertySpec.SOCKS5_GSSAPI_SERVICE_NAME.newProperty(
-					gssapiServiceName));
+			properties.add(cast(
+					PropertySpec.SOCKS5_GSSAPI_SERVICE_NAME.newProperty(
+							gssapiServiceName)));
 		}
 		if (settings.containsNondefaultValue(
 				SettingSpec.CHAINING_SOCKS5_USERNAME_PASSWORD)) {
 			UsernamePassword usernamePassword = settings.getLastValue(
 					SettingSpec.CHAINING_SOCKS5_USERNAME_PASSWORD);
 			if (usernamePassword != null) {
-				properties.add(PropertySpec.SOCKS5_USERNAME.newProperty(
-						usernamePassword.getUsername()));
-				properties.add(PropertySpec.SOCKS5_PASSWORD.newProperty(
-						usernamePassword.getEncryptedPassword()));			
+				properties.add(cast(PropertySpec.SOCKS5_USERNAME.newProperty(
+						usernamePassword.getUsername())));
+				properties.add(cast(PropertySpec.SOCKS5_PASSWORD.newProperty(
+						usernamePassword.getEncryptedPassword())));			
 			}
 		}
-		return castProperties(properties);		
+		return properties;		
 	}
 	
 	private SocksClient newSocksClient() {
@@ -165,103 +163,105 @@ final class ExternalNetFactory extends NetFactory {
 	
 	private List<Property<Object>> newSocksClientProperties() {
 		Settings settings = this.configuration.getSettings();
-		List<Property<? extends Object>> properties = 
-				new ArrayList<Property<? extends Object>>();
+		List<Property<Object>> properties = new ArrayList<Property<Object>>();
 		if (settings.containsNondefaultValue(SettingSpec.CHAINING_BIND_HOST)) {
 			Host bindHost = settings.getLastValue(
 					SettingSpec.CHAINING_BIND_HOST);
-			properties.add(PropertySpec.BIND_HOST.newProperty(bindHost));
+			properties.add(cast(PropertySpec.BIND_HOST.newProperty(bindHost)));
 		}
 		if (settings.containsNondefaultValue(
 				SettingSpec.CHAINING_CONNECT_TIMEOUT)) {
 			PositiveInteger connectTimeout = settings.getLastValue(
 					SettingSpec.CHAINING_CONNECT_TIMEOUT);
-			properties.add(PropertySpec.CONNECT_TIMEOUT.newProperty(
-					connectTimeout));
+			properties.add(cast(PropertySpec.CONNECT_TIMEOUT.newProperty(
+					connectTimeout)));
 		}
 		if (settings.containsNondefaultValue(
 				SettingSpec.CHAINING_SOCKET_SETTINGS)) {
 			SocketSettings socketSettings = settings.getLastValue(
 					SettingSpec.CHAINING_SOCKET_SETTINGS);
-			properties.add(PropertySpec.SOCKET_SETTINGS.newProperty(
-					socketSettings));
+			properties.add(cast(PropertySpec.SOCKET_SETTINGS.newProperty(
+					socketSettings)));
 		}
-		return castProperties(properties);		
+		return properties;		
 	}
 	
 	private List<Property<Object>> newSocksClientSslProperties() {
 		Settings settings = this.configuration.getSettings();
-		List<Property<? extends Object>> properties = 
-				new ArrayList<Property<? extends Object>>();
+		List<Property<Object>> properties = new ArrayList<Property<Object>>();
 		if (settings.containsNondefaultValue(
 				SettingSpec.CHAINING_SSL_ENABLED)) {
 			Boolean sslEnabled = settings.getLastValue(
 					SettingSpec.CHAINING_SSL_ENABLED);
-			properties.add(PropertySpec.SSL_ENABLED.newProperty(sslEnabled));
+			properties.add(cast(PropertySpec.SSL_ENABLED.newProperty(
+					sslEnabled)));
 		}
 		if (settings.containsNondefaultValue(
 				SettingSpec.CHAINING_SSL_ENABLED_CIPHER_SUITES)) {
 			Strings sslEnabledCipherSuites = settings.getLastValue(
 					SettingSpec.CHAINING_SSL_ENABLED_CIPHER_SUITES);
-			properties.add(PropertySpec.SSL_ENABLED_CIPHER_SUITES.newProperty(
-					sslEnabledCipherSuites));
+			properties.add(cast(
+					PropertySpec.SSL_ENABLED_CIPHER_SUITES.newProperty(
+							sslEnabledCipherSuites)));
 		}
 		if (settings.containsNondefaultValue(
 				SettingSpec.CHAINING_SSL_ENABLED_PROTOCOLS)) {
 			Strings sslEnabledProtocols = settings.getLastValue(
 					SettingSpec.CHAINING_SSL_ENABLED_PROTOCOLS);
-			properties.add(PropertySpec.SSL_ENABLED_PROTOCOLS.newProperty(
-					sslEnabledProtocols));			
+			properties.add(cast(PropertySpec.SSL_ENABLED_PROTOCOLS.newProperty(
+					sslEnabledProtocols)));			
 		}
 		if (settings.containsNondefaultValue(
 				SettingSpec.CHAINING_SSL_KEY_STORE_FILE)) {
 			File sslKeyStoreFile = settings.getLastValue(
 					SettingSpec.CHAINING_SSL_KEY_STORE_FILE);
-			properties.add(PropertySpec.SSL_KEY_STORE_FILE.newProperty(
-					sslKeyStoreFile));
+			properties.add(cast(PropertySpec.SSL_KEY_STORE_FILE.newProperty(
+					sslKeyStoreFile)));
 		}
 		if (settings.containsNondefaultValue(
 				SettingSpec.CHAINING_SSL_KEY_STORE_PASSWORD)) {
 			EncryptedPassword sslKeyStorePassword = settings.getLastValue(
 					SettingSpec.CHAINING_SSL_KEY_STORE_PASSWORD);
-			properties.add(PropertySpec.SSL_KEY_STORE_PASSWORD.newProperty(
-					sslKeyStorePassword));
+			properties.add(cast(PropertySpec.SSL_KEY_STORE_PASSWORD.newProperty(
+					sslKeyStorePassword)));
 		}
 		if (settings.containsNondefaultValue(
 				SettingSpec.CHAINING_SSL_KEY_STORE_TYPE)) {
 			String sslKeyStoreType = settings.getLastValue(
 					SettingSpec.CHAINING_SSL_KEY_STORE_TYPE);
-			properties.add(PropertySpec.SSL_KEY_STORE_TYPE.newProperty(
-					sslKeyStoreType));
+			properties.add(cast(PropertySpec.SSL_KEY_STORE_TYPE.newProperty(
+					sslKeyStoreType)));
 		}
 		if (settings.containsNondefaultValue(
 				SettingSpec.CHAINING_SSL_PROTOCOL)) {
 			String sslProtocol = settings.getLastValue(
 					SettingSpec.CHAINING_SSL_PROTOCOL);
-			properties.add(PropertySpec.SSL_PROTOCOL.newProperty(sslProtocol));
+			properties.add(cast(PropertySpec.SSL_PROTOCOL.newProperty(
+					sslProtocol)));
 		}
 		if (settings.containsNondefaultValue(
 				SettingSpec.CHAINING_SSL_TRUST_STORE_FILE)) {
 			File sslTrustStoreFile = settings.getLastValue(
 					SettingSpec.CHAINING_SSL_TRUST_STORE_FILE);
-			properties.add(PropertySpec.SSL_TRUST_STORE_FILE.newProperty(
-					sslTrustStoreFile));
+			properties.add(cast(PropertySpec.SSL_TRUST_STORE_FILE.newProperty(
+					sslTrustStoreFile)));
 		}
 		if (settings.containsNondefaultValue(
 				SettingSpec.CHAINING_SSL_TRUST_STORE_PASSWORD)) {
 			EncryptedPassword sslTrustStorePassword = settings.getLastValue(
 					SettingSpec.CHAINING_SSL_TRUST_STORE_PASSWORD);
-			properties.add(PropertySpec.SSL_TRUST_STORE_PASSWORD.newProperty(
-					sslTrustStorePassword));
+			properties.add(cast(
+					PropertySpec.SSL_TRUST_STORE_PASSWORD.newProperty(
+							sslTrustStorePassword)));
 		}
 		if (settings.containsNondefaultValue(
 				SettingSpec.CHAINING_SSL_TRUST_STORE_TYPE)) {
 			String sslTrustStoreType = settings.getLastValue(
 					SettingSpec.CHAINING_SSL_TRUST_STORE_TYPE);
-			properties.add(PropertySpec.SSL_TRUST_STORE_TYPE.newProperty(
-					sslTrustStoreType));
+			properties.add(cast(PropertySpec.SSL_TRUST_STORE_TYPE.newProperty(
+					sslTrustStoreType)));
 		}
-		return castProperties(properties);		
+		return properties;		
 	}
 	
 }

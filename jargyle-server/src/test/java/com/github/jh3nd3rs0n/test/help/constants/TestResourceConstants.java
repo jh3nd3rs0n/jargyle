720ce3813ab30a55ead0464c328336d820989df1
Jonathan K. Henderson <j0n4th4n.h3nd3rs0n@gmail.com>
2023-09-02 09:20:34 -0400

    consolidated jargyle-server-configrepo-impl into jargyle-server

--- jargyle-server/src/test/java/com/github/jh3nd3rs0n/test/help/constants/TestResourceConstants.java
+++ jargyle-server/src/test/java/com/github/jh3nd3rs0n/test/help/constants/TestResourceConstants.java
@@ -4,6 +4,12 @@ import com.github.jh3nd3rs0n.test.help.TestResource;
 
 public final class TestResourceConstants {
 	
+	public static final TestResource JARGYLE_SERVER_CONFIGREPO_IMPL_CONFIGURATION_FILE = new TestResource(
+			"com/github/jh3nd3rs0n/jargyle/server/configrepo/impl/configuration.xml");
+	
+	public static final TestResource JARGYLE_SERVER_CONFIGREPO_IMPL_EMPTY_CONFIGURATION_FILE = new TestResource(
+			"com/github/jh3nd3rs0n/jargyle/server/configrepo/impl/empty_configuration.xml");
+	
 	public static final TestResource JARGYLE_SERVER_SOCKS5_USERPASSAUTH_USERREPO_IMPL_ADDED_USER_TO_USERS_CSV_FILE = new TestResource(
 			"com/github/jh3nd3rs0n/jargyle/server/socks5/userpassauth/userrepo/impl/added_user_to_users.csv");
 	

c7807f5986ecfdf1668156e4638faa354e8b4e0e
Jonathan K. Henderson <j0n4th4n.h3nd3rs0n@gmail.com>
2023-10-19 14:06:17 -0400

    added/improved module exports

--- jargyle-internal/src/main/java/module-info.java
+++ jargyle-internal/src/main/java/module-info.java
@@ -1,10 +1,45 @@
 module com.github.jh3nd3rs0n.jargyle.internal {
 	requires org.slf4j;	
-	exports com.github.jh3nd3rs0n.jargyle.internal.annotation;
-	exports com.github.jh3nd3rs0n.jargyle.internal.lang;
-	exports com.github.jh3nd3rs0n.jargyle.internal.logging;
-	exports com.github.jh3nd3rs0n.jargyle.internal.net;
-	exports com.github.jh3nd3rs0n.jargyle.internal.net.ssl;
-	exports com.github.jh3nd3rs0n.jargyle.internal.regex;
-	exports com.github.jh3nd3rs0n.jargyle.internal.security;
+	exports com.github.jh3nd3rs0n.jargyle.internal.annotation to 
+	com.github.jh3nd3rs0n.jargyle.cli, 
+	com.github.jh3nd3rs0n.jargyle.client, 
+	com.github.jh3nd3rs0n.jargyle.common, 
+	com.github.jh3nd3rs0n.jargyle.server, 
+	com.github.jh3nd3rs0n.jargyle.transport;
+	exports com.github.jh3nd3rs0n.jargyle.internal.lang to
+	com.github.jh3nd3rs0n.jargyle.cli, 
+	com.github.jh3nd3rs0n.jargyle.client, 
+	com.github.jh3nd3rs0n.jargyle.common, 
+	com.github.jh3nd3rs0n.jargyle.server, 
+	com.github.jh3nd3rs0n.jargyle.transport;
+	exports com.github.jh3nd3rs0n.jargyle.internal.logging to
+	com.github.jh3nd3rs0n.jargyle.cli, 
+	com.github.jh3nd3rs0n.jargyle.client, 
+	com.github.jh3nd3rs0n.jargyle.common, 
+	com.github.jh3nd3rs0n.jargyle.server, 
+	com.github.jh3nd3rs0n.jargyle.transport;
+	exports com.github.jh3nd3rs0n.jargyle.internal.net to 
+	com.github.jh3nd3rs0n.jargyle.cli, 
+	com.github.jh3nd3rs0n.jargyle.client, 
+	com.github.jh3nd3rs0n.jargyle.common, 
+	com.github.jh3nd3rs0n.jargyle.server, 
+	com.github.jh3nd3rs0n.jargyle.transport;
+	exports com.github.jh3nd3rs0n.jargyle.internal.net.ssl to
+	com.github.jh3nd3rs0n.jargyle.cli, 
+	com.github.jh3nd3rs0n.jargyle.client, 
+	com.github.jh3nd3rs0n.jargyle.common, 
+	com.github.jh3nd3rs0n.jargyle.server, 
+	com.github.jh3nd3rs0n.jargyle.transport;
+	exports com.github.jh3nd3rs0n.jargyle.internal.regex to
+	com.github.jh3nd3rs0n.jargyle.cli, 
+	com.github.jh3nd3rs0n.jargyle.client, 
+	com.github.jh3nd3rs0n.jargyle.common, 
+	com.github.jh3nd3rs0n.jargyle.server, 
+	com.github.jh3nd3rs0n.jargyle.transport;
+	exports com.github.jh3nd3rs0n.jargyle.internal.security to
+	com.github.jh3nd3rs0n.jargyle.cli, 
+	com.github.jh3nd3rs0n.jargyle.client, 
+	com.github.jh3nd3rs0n.jargyle.common, 
+	com.github.jh3nd3rs0n.jargyle.server, 
+	com.github.jh3nd3rs0n.jargyle.transport;
 }
--- jargyle-server/src/main/java/module-info.java
+++ jargyle-server/src/main/java/module-info.java
@@ -8,12 +8,9 @@ module com.github.jh3nd3rs0n.jargyle.server {
 	requires org.slf4j;	
 	exports com.github.jh3nd3rs0n.jargyle.server;
 	exports com.github.jh3nd3rs0n.jargyle.server.configrepo.impl;
-	// export Rules to RulesTest
-	exports com.github.jh3nd3rs0n.jargyle.server.internal.server to com.github.jh3nd3rs0n.jargyle.server.test.internal.server;
+	exports com.github.jh3nd3rs0n.jargyle.server.internal.server to com.github.jh3nd3rs0n.server.test;
 	exports com.github.jh3nd3rs0n.jargyle.server.socks5.userpassauth;
-	// export Pbkdf2WithHmacSha256HashedPassword to Pbkdf2WithHmacSha256HashedPasswordTest
-	exports com.github.jh3nd3rs0n.jargyle.server.socks5.userpassauth.internal.hashedpass.impl to com.github.jh3nd3rs0n.jargyle.server.test.socks5.userpassauth.internal.hashedpass.impl;
+	exports com.github.jh3nd3rs0n.jargyle.server.socks5.userpassauth.internal.hashedpass.impl to com.github.jh3nd3rs0n.server.test;
 	exports com.github.jh3nd3rs0n.jargyle.server.socks5.userpassauth.userrepo.impl;
-	// export UsersCsvFileConversionHelper to UsersCsvFileConversionHelperTest
-	exports com.github.jh3nd3rs0n.jargyle.server.socks5.userpassauth.userrepo.impl.internal.users.csv.bind to com.github.jh3nd3rs0n.jargyle.server.test.socks5.userpassauth.userrepo.impl.internal.users.csv.bind;
+	exports com.github.jh3nd3rs0n.jargyle.server.socks5.userpassauth.userrepo.impl.internal.users.csv.bind to com.github.jh3nd3rs0n.server.test;
 }

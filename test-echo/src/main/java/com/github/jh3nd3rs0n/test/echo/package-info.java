8a559c448e4f21851d9901ee9cb7fe52f4dff9a7
Jonathan K. Henderson <j0n4th4n.h3nd3rs0n@gmail.com>
2024-11-01 23:39:48 -0400

    updated package-info.java files to recent changes

--- test-echo/src/main/java/com/github/jh3nd3rs0n/test/echo/package-info.java
+++ test-echo/src/main/java/com/github/jh3nd3rs0n/test/echo/package-info.java
@@ -1,5 +1,6 @@
 /**
- * Provides classes for integration testing between the SOCKS client API and
- * the SOCKS server API.
+ * Provides classes of clients and servers that send/receive data and
+ * receive/send back the same data. They are used for testing between the
+ * SOCKS client API and the SOCKS server API.
  */
 package com.github.jh3nd3rs0n.test.echo;
